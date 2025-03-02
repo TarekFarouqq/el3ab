ba76894b6045e926c004913f29880bb76c30ddb9
tarekfarouqq <tarekahmedfarouq@gmail.com>
2025-03-02 14:18:13 +0200

    ijuhyg

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "", "", "Warm", "", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "", "", "", "", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
